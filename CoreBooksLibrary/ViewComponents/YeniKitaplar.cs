a9e8ba0873ee67d063dc9039d095eec195d420b6
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-04-06 02:51:13 +0300

    ViewComponent Kullanımına giriş yapıldı

---
+++ CoreBooksLibrary/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,30 @@
+﻿using CoreBooksLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreBooksLibrary.ViewComponents
+{
+    public class YeniKitaplar: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitaplistesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    ID=4,
+                    KitapAd="Korku",
+                    Yazar="Zweigh"
+                },
+                new Kitaplar
+                {
+                    ID=5,
+                    KitapAd="Anna Karanina",
+                    Yazar="Tolstoy"
+                }
+
+            };
+            return View(kitaplistesi);
+        }
+    }
+}
