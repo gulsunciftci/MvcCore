08d99b2856ca8f90e7f50d1fcddf6e130b5d974b
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-04-09 00:49:15 +0300

    Departmanlar Listelendi

--- CoreDepartment/Controllers/departmanlarController.cs
+++ CoreDepartment/Controllers/departmanlarController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using CoreDepartment.Models;
 namespace CoreDepartment.Controllers
 {
     public class departmanlarController : Controller
     {
+        Context c = new Context();
         public IActionResult Index()
         {
-            return View();
+            var degerler = c.Departmanlars.ToList();
+            return View(degerler);
         }
     }
 }
