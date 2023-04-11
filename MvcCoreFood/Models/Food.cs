2260a5ce557178c3af05a78b75629d304da9d115
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-04-11 21:21:17 +0300

    Food an Core Projesi Add Migration işlemleri yapıldı

--- MvcCoreFood/Models/Food.cs
+++ MvcCoreFood/Models/Food.cs
@@ -4,12 +4,13 @@
     {
         public int FoodID { get; set; }
         public string Name { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
         public string ImageURL { get; set; }
-        public string ThumbNailImageURL { get; set; }
         public int Stock { get; set; }
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
        
     }
 }
